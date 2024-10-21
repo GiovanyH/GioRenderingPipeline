fcadd574e4fa70d6f7e2291eaa28b78c1dac2cc6
includeGiovany <harbsgiovany@gmail.com>
2024-10-20 21:26:05 -0300

    TriangleDemo class creation

---
+++ src/main/java/giovany/TriangleDemo.java
@@ -0,0 +1,15 @@
+package giovany;
+
+/**
+ *
+ * @author henri
+ */
+public class TriangleDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
